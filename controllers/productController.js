3f0dcf6a919303ca4580fec8a9cee6b3388ee5ab
uyashaISHINI <uyashaishini@gmail.com>
2024-12-18 12:47:26 +0530

    Connect the productRouter with index.js 😊

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,31 @@
-import Product from "../models/product.js";
+import product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 export function createProduct(req,res){
-    
+    if(!isAdmin(req)){
+        res.json({
+            message : "Please login as administrator to add products"
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message : "Product created"
+        })
+    }).catch((error)=>{
+        res.json({
+            message : error
+        })
+    })
+}
+
+export function getProducts(req,res){
+    product.find({}).then((products)=>{
+        res.json(products)
+    })
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
+import productRouter from './routes/productRouter.js';
 dotenv.config()
 
 const app = express();
@@ -37,6 +38,7 @@ app.use(
 )
 
 app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
 
 app.listen(
     5000,
