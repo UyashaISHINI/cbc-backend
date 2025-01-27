bf9da56b7f163ee4015f331fda7dc23e3cd35144
uyashaISHINI <uyashaishini@gmail.com>
2025-01-28 00:26:29 +0530

    Google login is created

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config()
 
 export function createUser(req,res){
@@ -110,5 +111,65 @@ export function isCustomer(req){
 
     return true
 }
+
+export async function googleLogin(req,res){
+    const token = req.body.token
+    try{
+        const response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo',{
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+        })
+        const email = response.data.email
+        const usersList = await User.find({email:email})
+        if(usersList.length >0){
+            const user =usersList[0]
+            const token = jwt.sign({
+                email : user.email,
+                firstName : user.firstName,
+                lastName : user.lastName,
+                isBlocked : user.isBlocked,
+                type : user.type,
+                profilePicture : user.profilePicture
+            }, process.env.SECRET)
+            res.json({
+                message : "User Logged in",
+                token : token,
+                user:{
+                    firstName : user.firstName,
+                    lastName : user.lastName,
+                    type : user.type,
+                    profilePicture : user.profilePicture,
+                    email : user.email
+                }
+            })
+        }else{
+            const newUserData = {
+                email :email,
+                firstName : response.data.given_name,
+                lastName : response.data.family_name,
+                type : "customer",
+                password : "fffffff",
+                profilePicture : response.data.picture
+            }
+            const user = new User(newUserData)
+            user.save().then(()=>{
+                res.json({
+                    message : "User Created"
+                })
+            }).catch((error)=>{
+                res.json({
+                    message:"User not created"
+                })
+            })
+        }
+    }catch(e){
+        res.json({
+            message : "Google login failed"
+            
+        })
+    }
+}
+
 // admin  "email": "dinuli@123.com", "password": "1234",
 // customer =  "Sara.doe@example.com"","password": "123",
